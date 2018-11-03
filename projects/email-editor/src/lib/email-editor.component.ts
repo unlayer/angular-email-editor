a507f95ee50bec8eff088c8f3e8db29fd079c55b
Umair Siddique <umairsiddique@gmail.com>
2018-11-03 13:11:55 -0700

    Initial wrapper component for unlayer email editor.

--- projects/email-editor/src/lib/email-editor.component.ts
+++ projects/email-editor/src/lib/email-editor.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+
+declare module unlayer {
+  function init(object);
+}
 
 @Component({
-  selector: 'lib-email-editor',
-  template: `
-    <p>
-      email-editor works!
-    </p>
-  `,
-  styles: []
+  // tslint:disable-next-line:component-selector
+  selector: 'email-editor',
+  templateUrl: './email-editor.component.html',
+  styleUrls: ['./email-editor.component.css']
 })
-export class EmailEditorComponent implements OnInit {
+export class EmailEditorComponent implements OnInit, AfterViewInit {
+
+  @Input() projectId: number;
+  @Input() templateId: number;
 
   constructor() { }
 
   ngOnInit() {
+    const unlayerScript = document.createElement('script');
+    unlayerScript.setAttribute('src', '//editor.unlayer.com/embed.js');
+    unlayerScript.onload = () => {
+      this.loadEditor();
+    };
+    document.head.appendChild(unlayerScript);
+  }
+
+  ngAfterViewInit() {
+  }
+
+  protected loadEditor() {
+    unlayer.init({
+      id: 'editor',
+      projectId: this.projectId,
+      templateId: this.templateId,
+      displayMode: 'email',
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmailEditorModule } from 'email-editor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EmailEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
