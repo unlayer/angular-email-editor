98109e41a1198d5c51bfc13edf0b0c09c096ed55
Ali Padron <alijesus29@gmail.com>
2021-02-07 00:12:41 -0400

    Angular updated to version 11

--- src/app/dashboard/design-edit/design-edit.component.spec.ts
+++ src/app/dashboard/design-edit/design-edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DesignEditComponent } from './design-edit.component';
 
@@ -6,7 +6,7 @@ describe('DesignEditComponent', () => {
   let component: DesignEditComponent;
   let fixture: ComponentFixture<DesignEditComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DesignEditComponent ]
     })
