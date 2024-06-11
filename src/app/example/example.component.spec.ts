7d385b110813b48c4b55ec8b2dedcd8a76f69165
Omer Zahid Bajwa <omerzahidbajwa@gmail.com>
2024-06-11 13:06:31 +0500

    fix npm test

--- src/app/example/example.component.spec.ts
+++ src/app/example/example.component.spec.ts
@@ -1,25 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ExampleComponent } from './example.component';
+import { EmailEditorModule } from 'email-editor';
 
 describe('ExampleComponent', () => {
-  let component: ExampleComponent;
-  let fixture: ComponentFixture<ExampleComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ExampleComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ExampleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ExampleComponent],
+      imports: [EmailEditorModule]
+    }).compileComponents();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(ExampleComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
